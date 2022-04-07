164c40fcad3cd4669331ef0f350caea94ae6d369
Pieter Ghysels <pghysels@lbl.gov>
2022-04-06 18:16:05 -0700

    Add some more comments in the example

--- strumpack/sparse.cpp
+++ strumpack/sparse.cpp
@@ -38,18 +38,12 @@ typedef int integer;
 using namespace strumpack;
 
 int main(int argc, char* argv[]) {
-  int thread_level;
-  //MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &thread_level);
+  int thread_level, myrank;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &thread_level);
-  int myrank;
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   if (thread_level != MPI_THREAD_FUNNELED && myrank == 0)
     std::cout << "MPI implementation does not support MPI_THREAD_FUNNELED"
               << std::endl;
-  // if (thread_level != MPI_THREAD_MULTIPLE && myrank == 0)
-  //   std::cout << "MPI implementation does not support MPI_THREAD_MULTIPLE,"
-  //     " which might be needed for pt-scotch!" << std::endl;
-
   {
     int n = 30, nrhs = 1;
     if (argc > 1) n = atoi(argv[1]); // get grid size
@@ -60,12 +54,28 @@ int main(int argc, char* argv[]) {
       std::cout << "solving 3D " << n << "^3 Poisson problem"
                 << " with " << nrhs << " right hand sides" << std::endl;
 
+    // Create the main solver object, using an MPI communicator.
     StrumpackSparseSolverMPIDist<scalar,integer> spss(MPI_COMM_WORLD);
+
+    // The matching phase finds a column permutation that maximizes
+    // the diagonal elements. Since the 3D Poisson problem is already
+    // diagonally dominant, we can disable this matching.
     spss.options().set_matching(MatchingJob::NONE);
+
+    // A fill reducing ordering ordering is a symmtric permutation of
+    // the matrix that minimizes the fill in the sparse triangular
+    // factors. Since the problem here is defined on a regular mesh,
+    // we can use a simple geometric nested dissection algorithm (see
+    // also below spss.reorder(n, n, n) where the mesh dimensions are
+    // specified). For general sparse matrices, use any other option,
+    // such as the default ReorderingStrategy::METIS, or the parallel
+    // ReorderingStrategy::PARMETIS (if STRUMPACK was configured with
+    // PARMETIS support).
     spss.options().set_reordering_method(ReorderingStrategy::GEOMETRIC);
     spss.options().set_from_command_line(argc, argv);
 
-    // TODO directly build the distributed sparse matrix
+    // construct a sparse matrix from a simple 7 point stencil, with
+    // zero boundary conditions
     CSRMatrix<scalar,integer> A;
     if (!myrank) {
       int n2 = n * n;
@@ -94,18 +104,42 @@ int main(int argc, char* argv[]) {
           }
       A.set_symm_sparse();
     }
+    // This scatters the sparse matrix A from the root over all the
+    // ranks, using a 1D block row distribution, see
+    // https://portal.nersc.gov/project/sparse/strumpack/master/sparse_example_usage.html#autotoc_md9
     CSRMatrixMPI<scalar,integer> Adist(&A, MPI_COMM_WORLD, true);
+    // delete sequential sparse matrix (on the root)
     A = CSRMatrix<scalar,integer>();
 
     auto n_local = Adist.local_rows();
     DenseMatrix<scalar> b(n_local, nrhs), x(n_local, nrhs),
       x_exact(n_local, nrhs);
+
+    // construct a random exact solution
     x_exact.random();
+
+    // compute a right hand-side corresponding to exact solution
+    // x_exact
     Adist.spmv(x_exact, b);
 
+
+    // One can also directly pass the CSR rowptr, colind, and value
+    //   arrays, see
+    //   https://portal.nersc.gov/project/sparse/strumpack/master/classstrumpack_1_1SparseSolver.html
     spss.set_matrix(Adist);
+
+    // For geometric nested dissection, the the mesh dimensions n x n
+    // x n (and separator width if not 1) need to be provided. For
+    // other fill-reducing orderings, such as the default
+    // ReorderingStrategy::GEOMETRIC, just call spss.reorder();
     spss.reorder(n, n, n);
+
+    // the actual numerical factorization phase. If reorder() was not
+    // already called, it will be called by factor internally.
     spss.factor();
+
+    // solve a linear system Ax=b for x. If factor was not already
+    // called, then it will be called by solve internally.
     spss.solve(b, x);
 
     auto scaled_res = Adist.max_scaled_residual(x, b);
@@ -118,7 +152,6 @@ int main(int argc, char* argv[]) {
                 << relerr << std::endl;
     }
   }
-  //TimerList::Finalize();
   scalapack::Cblacs_exit(1);
   MPI_Finalize();
   return 0;
