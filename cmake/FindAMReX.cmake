f2fce0400a42a4e70adebbfe6c87369259fd681b
David J. Gardner <gardner48@llnl.gov>
2022-04-28 11:22:16 -0700

    Initial AMReX + SUNDIALS example building on CPU #10
    Requires fix to SUNDIALS version in AMReXConfig.cmake.in see
    AMReX-Codes/amrex#2743

---
+++ cmake/FindAMReX.cmake
@@ -0,0 +1,9 @@
+find_package(AMReX REQUIRED
+  HINTS ${AMREX_DIR} $ENV{AMREX_DIR} ${CMAKE_PREFIX_PATH}
+  NO_DEFAULT_PATH)
+
+if(NOT TARGET XSDK::AMReX)
+  add_library(XSDK_AMREX INTERFACE)
+  target_link_libraries(XSDK_AMREX INTERFACE AMReX::amrex)
+  add_library(XSDK::AMReX ALIAS XSDK_AMREX)
+endif()
