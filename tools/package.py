ca1eed94c2358461de08cf942a00d9f1bc10dbcc
Piotr Luszczek <luszczek@icl.utk.edu>
2022-05-01 13:26:33 -0400

    Add Spack package changes for PLASMA example

--- tools/package.py
+++ tools/package.py
@@ -1,10 +1,11 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
 from spack import *
+
 
 class XsdkExamples(CMakePackage):
     """xSDK Examples show usage of libraries in the xSDK package."""
@@ -16,12 +17,14 @@ class XsdkExamples(CMakePackage):
     maintainers = ['acfisher', 'balay', 'balos1', 'luszczek']
 
     version('develop', branch='master')
+    version('0.3.0', sha256='efd16e0a49195acef9cfc8f288b42e517461cc04fef1cd019ca4ccea0cd20cc0')
     version('0.2.0', sha256='cf26e3a16a83eba6fb297fb106b0934046f17cf978f96243b44d9d17ad186db6')
     version('0.1.0', sha256='d24cab1db7c0872b6474d69e598df9c8e25d254d09c425fb0a6a8d6469b8018f')
 
     variant('cuda', default=False, description='Compile CUDA examples')
 
-    depends_on('xsdk+cuda ^mfem+cuda', when='+cuda')
+    depends_on('xsdk+cuda ^mfem+cuda', 'xsdk+cuda', when='+cuda')
+    depends_on('xsdk@0.7.0', when='@0.3.0')
     depends_on('xsdk@0.6.0', when='@0.2.0')
     depends_on('xsdk@0.5.0', when='@0.1.0')
 
@@ -32,12 +35,16 @@ class XsdkExamples(CMakePackage):
             '-DMETIS_INCLUDE_DIRS=%s' % spec['metis'].prefix.include,
             '-DMETIS_LIBRARY=%s' % spec['metis'].libs,
             '-DMPI_DIR=%s' % spec['mpi'].prefix,
+            '-DBLASPP_DIR=%s' % spec['blaspp'].prefix,
             '-DSUNDIALS_DIR=%s'      % spec['sundials'].prefix,
             '-DHYPRE_DIR=%s'         % spec['hypre'].prefix,
             '-DHYPRE_INCLUDE_DIR=%s' % spec['hypre'].prefix.include,
+            '-DLAPACKPP_DIR=%s'      % spec['lapackpp'].prefix,
             '-DPETSC_DIR=%s'         % spec['petsc'].prefix,
             '-DPETSC_INCLUDE_DIR=%s' % spec['petsc'].prefix.include,
             '-DPETSC_LIBRARY_DIR=%s' % spec['petsc'].prefix.lib,
+            '-DPLASMA_DIR=%s'        % spec['plasma'].prefix,
+            '-DSLATE_DIR=%s'         % spec['slate'].prefix,
             '-DSUPERLUDIST_DIR=%s' % spec['superlu-dist'].prefix,
             '-DSUPERLUDIST_INCLUDE_DIR=%s' %
             spec['superlu-dist'].prefix.include,
@@ -52,6 +59,10 @@ class XsdkExamples(CMakePackage):
             # allow use of default `find_package(Ginkgo)`
             '-DCMAKE_PREFIX_PATH=%s/cmake' % spec['ginkgo'].prefix.include.lib
         ]
+        if '+cuda' in spec:
+            args.extend([
+                '-DENABLE_CUDA=ON'
+            ])
         if 'trilinos' in spec:  # if trilinos variant was activated for xsdk
             args.extend([
                 '-DTRILINOS_DIR_PATH=%s' % spec['trilinos'].prefix,
